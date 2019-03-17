aa8d132c8996c75f6cd7ce4226a1c544c0c74dda
DanielHammami <daniel.hammami@yahoo.com>
2019-03-17 16:36:52 +0100

    Score and End of the Game

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ window.onload = function()
     var applee
     var widthInBlocks = canvasWidth/blockSize
     var heightInBlocks = canvasHeight/blockSize
+    var score
 
     init()
 
@@ -22,6 +23,7 @@ window.onload = function()
         ctx = canvas.getContext('2d')
         snakee = new Snake([[6, 4], [5, 4], [4, 4], [3, 4]], 'right')
         applee = new Apple([10, 10])
+        score = 0
         refreshCanvas()
     }
 
@@ -31,12 +33,13 @@ window.onload = function()
 
         if(snakee.checkCollision())
         {
-            // Game over
+            gameOver()
         }
         else
         {
             if(snakee.isEatingApple(applee))
             {
+                score++
                 snakee.ateApple = true
 
                 do
@@ -49,10 +52,34 @@ window.onload = function()
             ctx.clearRect(0, 0, canvasWidth, canvasHeight)
             snakee.draw()
             applee.draw()
+            drawScore()
             setTimeout(refreshCanvas, delay)
         }
     }
+
+    function gameOver()
+    {
+        ctx.save()
+        ctx.fillText('Game Over', 5, 15)
+        ctx.fillText('Appuyer sur la touche Espace pour rejouer', 5, 30)
+        ctx.restore()
+    }
     
+    function restart()
+    {
+        snakee = new Snake([[6, 4], [5, 4], [4, 4], [3, 4]], 'right')
+        applee = new Apple([10, 10])
+        score = 0
+        refreshCanvas()
+    }
+
+    function drawScore()
+    {
+        ctx.save()
+        ctx.fillText(score.toString(), 5, canvasHeight - 5)
+        ctx.restore()
+    }
+
     function drawBlock(ctx, position) 
     {
         var x = position[0] * blockSize
@@ -241,6 +268,9 @@ window.onload = function()
             case 40:
                 newDirection = 'down'
                 break
+            case 32:
+                restart()
+                return
             default:
                 return
         }
